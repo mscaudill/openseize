dc2ba57e3a1376aa9bd76633d9c42ca7440a9694
mscaudill <mscaudill@gmail.com>
2021-09-02 15:57:32 -0500

    added generator method to reader. This makes the EEG object of old now obsolete as the reader can perform all of its task without pretending to be a ndarray

--- io/edf.py
+++ io/edf.py
@@ -2,6 +2,8 @@ import numpy as np
 
 from openseize.io.headers import EDFHeader
 
+from openseize.mixins import ViewInstance
+
 def open_edf(path, mode):
     """Opens an edf file at path for reading or writing. 
 
@@ -22,11 +24,11 @@ def open_edf(path, mode):
         raise ValueError("invalid mode '{}'".format(mode))
 
 
-class FileManager:
+class FileManager(ViewInstance):
     """A context manager for ensuring files are closed at the conclusion
     of reading or writing or in case of any raised exceptions.
 
-    This class defines an interface and therefore can not be instantiated.
+    This class defines a partial interface and can not be instantiated.
     """
 
     def __init__(self, path, mode):
@@ -36,7 +38,7 @@ class FileManager:
             msg = '{} class cannot be instantiated'
             print('msg'.format(type(self).__name__))
         self.path = path
-        self.fobj = open(path, mode)
+        self._fobj = open(path, mode)
 
     def __enter__(self):
         """Return instance as target variable of this context."""
@@ -52,7 +54,7 @@ class FileManager:
     def close(self):
         """Close this instance's opened file object."""
 
-        self.fobj.close()
+        self._fobj.close()
 
 
 class Reader(FileManager):
@@ -77,9 +79,14 @@ class Reader(FileManager):
         super().__init__(path, mode='rb')
         self.header = EDFHeader(path)
 
-    def _decipher(self, arr, axis=-1):
+    def _decipher(self, arr, channels, axis=-1):
         """Deciphers an array of integers read from an EDF into an array of
         voltage float values.
+
+        Args:
+            arr (ndarry):           2D-array of int type
+            channels (list):        list of channels to decipher
+            axis (int):             sample axis of arr
 
         The physical values p are linearly mapped from the digital values d:
         p = slope * d + offset, where the slope = 
@@ -88,25 +95,27 @@ class Reader(FileManager):
         Returns: ndarray with shape matching input shape & float64 dtype
         """
 
-        slopes = self.header.slopes[self.header.channels]
-        offsets = self.header.offsets[self.header.channels]
+        slopes = self.header.slopes[channels]
+        offsets = self.header.offsets[channels]
         #expand to 2-D for broadcasting
-        slopes = np.expand_dims(slopes[self.header.channels], axis=axis)
-        offsets = np.expand_dims(offsets[self.header.channels], axis=axis)
+        slopes = np.expand_dims(slopes, axis=axis)
+        offsets = np.expand_dims(offsets, axis=axis)
         result = arr * slopes
         result += offsets
         return result
 
-    def _find_records(self, start, stop):
+    def _find_records(self, start, stop, channels):
         """Returns tuples (one per signal) of start, stop record numbers
         that include the start, stop sample numbers
 
         Args:
             start (int):                start of sample range to read
             stop (int):                 stop of sample range to read
+            channels (list):            list of channels to return record
+                                        numbers for
         """
 
-        spr = np.array(self.header.samples_per_record)[self.header.channels]
+        spr = np.array(self.header.samples_per_record)[channels]
         starts = start // spr
         stops = np.ceil(stop / spr).astype('int')
         return list(zip(starts, stops))
@@ -115,29 +124,33 @@ class Reader(FileManager):
         """Reads all samples from the ath to bth record.
 
 
-        Returns: a 1D array of length (b-a) * sum(samples_per_record)
+        Returns: a 2D array of shape (b-a) x sum(samples_per_record)
         
         Note: Returns samples upto the end of the file if b exceeds the
         number of records as np.fromfile gracefully handles this for us
         """
 
         #move to file start
-        self.fobj.seek(0)
+        self._fobj.seek(0)
         cnt = b - a
         #each sample is represented as a 2-byte integer
         bytes_per_record = sum(self.header.samples_per_record) * 2
-        #return offset in bytes & num samples spanning a to b
+        #get offset in bytes & num samples spanning a to b
         offset = self.header.header_bytes + a * bytes_per_record
         nsamples = cnt * sum(self.header.samples_per_record)
-        return np.fromfile(self.fobj, dtype='<i2', count=nsamples, 
+        records = np.fromfile(self._fobj, dtype='<i2', count=nsamples, 
                            offset=offset)
+        #reshape the record to num_records x sum samples_per_rec
+        arr = records.reshape(cnt, sum(self.header.samples_per_record))
+        return arr
 
-    def read(self, start, stop):
-        """Returns samples from start to stop for all channels of this EDF.
+    def _read_array(self, start, stop, channels):
+        """Returns samples from start to stop for channels of this EDF.
 
         Args:
             start (int):            start sample to begin reading
             stop (int):             stop sample to end reading (exclusive)
+            channels (list):        channels to return samples for
 
         Returns: array of shape chs x samples with float64 dtype
         
@@ -145,28 +158,74 @@ class Reader(FileManager):
         as np.fromfile gracefully handles this for us
         """
 
-        #locate record ranges for chs & find unique ones to read
-        recs = self._find_records(start, stop)
-        urecs = set(recs)
-        #read each unique record range
-        reads = {urec: self._records(*urec) for urec in urecs}
-        #reshape each read to num_records x summed samples per rec
-        reads = {urec: arr.reshape(-1, sum(self.header.samples_per_record)) 
-                 for urec, arr in reads.items()}
+        #locate record endpts for each channel
+        rec_pts = self._find_records(start, stop, channels)
+        #read the data for each unique record endpt tuple
+        uniq_pts = set(rec_pts)
+        reads = {pts: self._records(*pts) for pts in uniq_pts}
         #perform final slicing and transform for each channel
-        result = []
-        for idx, (ch, rec) in enumerate(zip(self.header.channels, recs)):
-            #get preread array and slice out channel
-            arr = reads[rec]
+        result=[]
+        for ch, pts in zip(channels, rec_pts):
+            #get preread array and extract samples for this ch
+            arr = reads[pts]
             arr = arr[:, self.header.record_map[ch]].flatten()
-            #adjust start & stop relative to first read record & store
-            a = start - rec[0] * self.header.samples_per_record[ch]
+            #adjust start & stop relative to records start pt
+            a = start - pts[0] * self.header.samples_per_record[ch]
             b = a + (stop - start)
             result.append(arr[a:b])
-        result = np.stack(result, axis=0)
-        #decipher & return
-        result = self._decipher(result)
-        return result
+        res =  np.stack(result, axis=0)
+        #decipher and yield
+        return self._decipher(res, channels)
+
+    def _read_gen(self, start, channels, chunksize):
+        """A generator yielding arrays of data from an EDF file.
+
+        Args:
+            start (int):            start sample of returned data
+            channels (list):        list of channels to return data for
+            chunksize (int):        number of samples to return per iter
+          
+        Yields: arrays of shape chs x samples with float64 dtype
+        """
+
+        starts = range(start, max(self.header.samples), chunksize)
+        for start, stop in zip(starts, starts[1:]):
+            yield self._read_idx(start, stop, channels)
+
+    def read(self, start, stop=None, channels=None, chunksize=30e6):
+        """Reads samples from an edf file for the specified channels.
+
+        Depending on supplied arguments this function will return an array
+        of samples or a generator of arrays of samples.
+
+        Args:
+            start (int):            start sample to read
+            stop (int):             stop sample to read. If None return
+                                    a generator that sequentially yields 
+                                    arrays of shape (channels,chunksize)
+                                    until end of file
+            channels (list):        indices to return or yield data from
+                                    (Default None returns data on all
+                                    channels in EDF)
+            chunksize (int):        number of samples to return from
+                                    generator per iteration (Default is
+                                    30e6 samples). This value is ignored if
+                                    stop sample is provided.
+
+        Returns/Yields:
+            if stop is None, read yields arrays of chs x chunksize samples
+            if stop is int, returns an array of chs x (stop-start) samples 
+            array(s) yielded or returned are of dtype float64.
+        """
+
+        #use all channels if None and dispatch to read method
+        channels = self.header.channels if not channels else channels
+        if not stop:
+            #return generator
+            return self._read_gen(start, channels, int(chunksize))
+        else:
+            #return an array
+            return self._read_array(start, stop, channels)
 
 
 class Writer(FileManager):
@@ -177,7 +236,7 @@ class Writer(FileManager):
     """
 
     def __init__(self, path):
-        """ """
+        """Initialize this Writer with a write path."""
 
         super().__init__(path, mode='wb')
 
@@ -194,14 +253,14 @@ class Writer(FileManager):
              for k, v in self.header.items()}
         #build an edf bytemap dict and move to file start byte
         bmap = self.header.bytemap(self.header.num_signals)
-        self.fobj.seek(0)
+        self._fobj.seek(0)
         #encode each value list in header and write
         for values, bytelist in ((d[name], bmap[name][0]) for name in bmap):
             bvalues = [bytes(str(value), encoding='ascii').ljust(nbytes) 
                       for value, nbytes in zip(values, bytelist)]
             #join the byte strings and write
             bstring = b''.join(bvalues)
-            self.fobj.write(bstring)
+            self._fobj.write(bstring)
 
     def _encipher(self, arr, axis=-1):
         """Transform arr of float values to an array of 2-byte 
@@ -236,7 +295,7 @@ class Writer(FileManager):
         for ch in self.header.channels:
             samples = x[ch, :self.header.samples_per_record[ch]]
             byte_str = samples.tobytes()
-            self.fobj.write(byte_str)
+            self._fobj.write(byte_str)
 
     def _progress(self, idx):
         """Relays write progress during file writing."""
@@ -297,7 +356,7 @@ class Writer(FileManager):
         header = EDFHeader.from_dict(header)
         self._write_header(header)
         #Move to data records section
-        self.fobj.seek(header.header_bytes)
+        self._fobj.seek(header.header_bytes)
         for idx, record in enumerate(self._records(data, axis=axis)):
             self._progress(idx)
             self._write_record(record, axis=axis)
@@ -319,8 +378,13 @@ if __name__ == '__main__':
         res = infile.read(0,1000)
     """
 
+    """
     with open_edf(path2, 'wb') as outfile:
         outfile.write(header, data, axis=0)
+    """
 
     #fm = FileManager(path, 'rb')
+
+    gen = reader.read(start=0, channels=[0,1,2,3], chunksize=int(30e6))
+    arr = reader.read(start=0, stop=1, channels=[0,3])
 
