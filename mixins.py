2fc4ca3e5047eeec6bbc3b219c355adeb1177cdd
mscaudill <mscaudill@gmail.com>
2021-09-02 15:58:16 -0500

    initial commit of mixins to provide str aand repr representations to any instance

---
+++ mixins.py
@@ -0,0 +1,63 @@
+import inspect
+import pprint
+
+class ViewInstance:
+    """Mixin endowing inheritors with echo and print str representations."""
+
+    def _fetch_attributes(self):
+        """Returns a dict of all non-protected attrs."""
+
+        return {attr: val for attr, val in self.__dict__.items()
+                if not attr.startswith('_')}
+            
+    def _fetch_methods(self):
+        """Returns non-protected instance and class methods."""
+        
+        methods = dict(inspect.getmembers(self, inspect.ismethod))
+        return {name: m for name, m in methods.items() 
+                if not name.startswith('_')}
+
+    def _fetch_properties(self):
+        """Returns non_protected properties."""
+
+        def isproperty(item):
+            """Helper returing True if item is a property."""
+
+            return isinstance(item, property)
+
+        props = inspect.getmembers(type(self), isproperty)
+        return {name: getattr(self, name) for name,_ in props 
+                if not name.startswith('_')}
+
+    def __repr__(self):
+        """Returns the __init__'s signature as the echo representation.
+        
+        Returns: str
+        """
+
+        #build a signature and get its args and class name
+        signature = inspect.signature(self.__init__)
+        args = str(signature)
+        cls_name = type(self).__name__
+        return '{}{}'.format(cls_name, args)
+
+    def __str__(self):
+        """Returns this instances print representation."""
+
+        cls_name = type(self).__name__
+        #get the attributes & properties
+        attrs = self._fetch_attributes()
+        props = self._fetch_properties()
+        #add properties
+        attrs.update(props)
+        #build a pretty printed msg
+        msg_start = cls_name + ' Object\n' + '---Attributes & Properties---'
+        pp = pprint.PrettyPrinter(sort_dicts=False, compact=True)
+        msg_body = pp.pformat(attrs) 
+        msg_end = '\nType help({}) for full documentation'.format(cls_name)
+        return '\n'.join([msg_start, msg_body, msg_end])
+
+
+
+
+
