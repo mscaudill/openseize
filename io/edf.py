59c997c098cddec28450a6160f11ea700b9ea0e6
mscaudill <mscaudill@gmail.com>
2021-09-17 16:31:27 -0500

    factored file context manager out since it is now used in annotations

--- io/edf.py
+++ io/edf.py
@@ -2,6 +2,7 @@ import numpy as np
 from pathlib import Path
 
 from openseize.io.headers import EDFHeader
+from openseize.io.managers import FileManager
 from openseize.mixins import ViewInstance
 
 def open_edf(path, mode):
@@ -23,39 +24,6 @@ def open_edf(path, mode):
         return Writer(path)
     else:
         raise ValueError("invalid mode '{}'".format(mode))
-
-
-class FileManager(ViewInstance):
-    """A context manager for ensuring files are closed at the conclusion
-    of reading or writing or in case of any raised exceptions.
-
-    This class defines a partial interface and can not be instantiated.
-    """
-
-    def __init__(self, path, mode):
-        """Initialize this FileManager inheritor with a path and mode."""
-
-        if type(self) is FileManager:
-            msg = '{} class cannot be instantiated'
-            print('msg'.format(type(self).__name__))
-        self.path = Path(path)
-        self._fobj = open(path, mode)
-
-    def __enter__(self):
-        """Return instance as target variable of this context."""
-
-        return self
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        """Close this instances file object & propagate any error by 
-        returning None."""
-
-        self.close()
-
-    def close(self):
-        """Close this instance's opened file object."""
-
-        self._fobj.close()
 
 
 class Reader(FileManager):
