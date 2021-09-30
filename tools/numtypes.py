38d6e1b18ba6526f7b1faccb197f02406ee616e0
mscaudill <mscaudill@gmail.com>
2021-09-30 17:31:53 -0500

    documented the producers. Full testing to come later

--- tools/numtypes.py
+++ tools/numtypes.py
@@ -4,87 +4,134 @@ import numpy as np
 
 from openseize.mixins import ViewInstance
 
-def producer(data, chunksize, axis=-1, **kwargs):
-    """ """
+def producer(data, chunksize, axis=-1):
+    """Returns an iterable of numpy ndarrays from an ndarray, a sequence of
+    arrays or generator of arrays.
 
+    Args:
+        data (ndarray, Sequence, Generator):   an ndarray or object that
+                                               contains or yields ndarrays
+                                               (e.g. np.memmap, list of
+                                               ndarrays, or a generator of
+                                               ndarrays of any shape)
+        chunksize (int):                       len of each ndarray yielded
+                                               by this iterable along axis
+                                               parameter
+        axis (int):                            axis of data to partition
+                                               into chunks of chunksize
+        
+    Returns: a Producer iterable   
+    """
+
+    chunksize = int(chunksize)
     if isinstance(data, Generator):
-        return _FromGenerator(data, chunksize, axis, **kwargs)
+        return _ProducerFromGenerator(data, chunksize, axis)
     elif isinstance(data, np.ndarray):
-        return _FromArray(data, chunksize, axis, **kwargs)
+        return _ProducerFromArray(data, chunksize, axis)
     elif isinstance(data, Sequence):
         data = np.array(data)
-        return _FromArray(data, chunksize, axis, **kwargs)
+        return _ProducerFromArray(data, chunksize, axis)
     else:
         msg = 'unproducible type: {}'
         raise TypeError(msg.format(type(data)))
 
-class _FromArray(ViewInstance):
-    """ """
+
+class Producer(Iterable, ViewInstance):
+    """ABC defining concrete and required methods of all Producers.
+
+    Args:
+        data (ndarray, Sequence, Generator):   an ndarray or object that
+                                               contains or yields ndarrays
+                                               (e.g. np.memmap, list of
+                                               ndarrays, or a generator of
+                                               ndarrays of any shape)
+        chunksize (int):                       len of each ndarray yielded
+                                               by this iterable along axis
+                                               parameter
+        axis (int):                            axis of data to partition
+                                               into chunks of chunksize
+
+    As an ABC, this class cannot be instantiated. To create a producer
+    instance use producer() function call.
+    """
 
     def __init__(self, data, chunksize, axis, **kwargs):
-        """ """
+        """Concrete initializer for all Producer subclasses."""
 
         self.data = data
         self.chunksize = chunksize
         self.axis = axis
         self.__dict__.update(kwargs)
+        #ViewInstance will use Producer name not subclass name
         self.__class__.__name__ = 'Producer'
 
-    @property
+
+class _ProducerFromArray(Producer):
+    """A Producer of ndarrays from an ndarray."""
+
     def segments(self):
-        """ """
+        """Returns start, stop indices of span chunksize to apply along
+        this Producer's axis."""
 
         extent = self.data.shape[self.axis]
         starts = range(0, extent, self.chunksize)
         return itertools.zip_longest(starts, starts[1:], fillvalue=extent)
 
     def __iter__(self):
-        """ """
+        """Returns an iterator yielding ndarrays of chunksize along axis."""
 
+        #create an ndarray slice sequence
         slices = [slice(None)] * self.data.ndim
-        for segment in self.segments:
+        for segment in self.segments():
+            #update slice and fetch from data
             slices[self.axis] = slice(*segment)
             yield self.data[tuple(slices)]
 
 
-class _FromGenerator(ViewInstance):
-    """ """
- 
-    def __init__(self, data, chunksize, axis, **kwargs):
-        """ """
+class _ProducerFromGenerator(Producer):
+    """A Producer of ndarrays from a generator of ndarrays.
 
-        self.data = data
-        self.chunksize = chunksize
-        self.axis = axis
-        self.__dict__.update(kwargs)
-        self.__class__.__name__ = 'Producer'
-   
-    @property
-    def _datachunksize(self):
-        """ """
+    A generator of ndarrays will return arrays whose shape is determined
+    when the generator was created. This iterable moves over these arrays
+    collecting arrays until chunksize shape is reached along axis. Once
+    reached, iter yields a collected ndarray.
+    """
+ 
+    def _datastep(self):
+        """Returns the length of data generator's ndarrays along axis."""
         
-        #make 2 indpt generators
+        #create 2 indpt gens & overwrite data since tmp will advance it
         self.data, tmp = itertools.tee(self.data, 2)
+        #advance an indpt tmp generator by one step to get shape
         return next(tmp).shape[self.axis]
 
+    def segments(self):
+        """Returns an iterable of start, stop tuples of span chunksize."""
+
+        #since data gen len is unknowable create infinite segments
+        return zip(itertools.count(0, self.chunksize), 
+                   itertools.count(self.chunksize, self.chunksize))
+
     def __iter__(self):
-        """ """
+        """Returns an iterator yielding ndarrays of chunksize along axis."""
        
-        gsize = self._datachunksize
-        segments = zip(itertools.count(0, self.chunksize), 
-                       itertools.count(self.chunksize, self.chunksize))
-        for start, stop in segments:
-            #compute generator segments containing segment pts
-            gsegments = (start // gsize, stop // gsize + 1)
+        datastep = self._datastep()
+        for start, stop in self.segments():
+            #find the data segments containing this Producer segment
+            datasegments = (start // datastep, stop // datastep + 1)
+            #create 2 indpt generators from data so one can be sliced
             self.data, tmp = itertools.tee(self.data, 2)
-            g = itertools.islice(tmp, *gsegments)
-            arrs = [arr for arr in g]
+            sliced = itertools.islice(tmp, *datasegments)
+            arrs = [arr for arr in sliced]
+            #data exhaustion check
             if not arrs:
                 break
             arr = np.concatenate(arrs, axis=self.axis)
+            #index within start data segement where producer start occurs
+            idx = start - (start // datastep * datastep)
+            #slice collected data segments (arr) starting from index & yield
             slices = [slice(None)] * arr.ndim
-            leftbound = start - (start // gsize * gsize)
-            slices[self.axis] = slice(leftbound, leftbound + self.chunksize)
+            slices[self.axis] = slice(idx, idx + self.chunksize)
             yield arr[tuple(slices)]
 
 
@@ -101,10 +148,10 @@ if __name__ == '__main__':
     """
 
 
-    x = np.random.random((4,1110))
+    x = np.random.random((10, 4,1110))
     gen = (arr for arr in np.split(x, 10, axis=-1))
     gprod = producer(gen, chunksize=30, axis=-1)
-    y = np.concatenate([arr for arr in gprod], axis=1)
+    y = np.concatenate([arr for arr in gprod], axis=-1)
     print(np.allclose(x,y))
 
 
