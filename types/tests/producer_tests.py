6899b9e3a3f026962152dc1ba63f62fc23e963b9
mscaudill <mscaudill@gmail.com>
2021-10-06 16:27:17 -0500

    completed iter testing of array and gen producers for random large arrays

--- types/tests/producer_tests.py
+++ types/tests/producer_tests.py
@@ -4,8 +4,12 @@ from itertools import zip_longest
 
 from openseize.types.producer import producer
 
+def test_shape():
+    """ """
 
-def array_test():
+    pass
+
+def test_array():
     """Test if producer produces correct subarrays from a supplied array."""
     
     chunksize = 10028
@@ -20,30 +24,24 @@ def array_test():
         slicer[-1] = slice(*seg)
         #compare
         result = np.allclose(arr[tuple(slicer)], sub_arr)
-    print(np.all(result))
+    assert np.all(result)
 
-def gen_test():
-    """ """
+def test_gen():
+    """Test if producer produces correct subarrays from a generator."""
 
-    chunksize=2011
-    np.random.seed(0)
-    lens = np.random.randint(1000, high=40000, size=3)
-    print(lens)
-    arrs = [np.random.random((21, l)) for l in lens]
+    chunksize=2231
+    np.random.seed(9634)
+    #make sure to use subarrays of varying lens along chunking axis
+    lens = np.random.randint(2000, high=40000, size=50)
+    #keep the arrays for comparison and make a 1-time generator
+    arrs = [np.random.random((17, l)) for l in lens]
     gen = (arr for arr in arrs)
+    #create a producer from the generator
     pro = producer(gen, chunksize=chunksize, axis=-1)
-
-    pro_arrays = []
-    for arr in pro:
-        pro_arrays.append(arr)
+    #fetch and store the arrays from the producer
+    pro_arrays = [arr for arr in pro]
+    #create arrays for comparison from original and from producer
     arr = np.concatenate(arrs, axis=-1)
     pro_arr = np.concatenate(pro_arrays, axis=-1)
-    print(np.allclose(arr, pro_arr))
-    print(arr.shape, pro_arr.shape)
-    return arr, pro_arr
-
-if __name__ == '__main__':
-
-    arr, pro_arr = gen_test()
-
+    assert np.allclose(arr, pro_arr)
 
