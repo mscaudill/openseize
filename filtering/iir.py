29de02af2f2bcdd62dfac76f586bef8461b5c938
mscaudill <mscaudill@gmail.com>
2021-10-07 17:01:05 -0500

    working to clean up iir but need to complete filtfilt in numerical first. May end up only supporting SOS fmt for now.

--- filtering/iir.py
+++ filtering/iir.py
@@ -1,11 +1,11 @@
 import abc
 import numpy as np
-import matplotlib.pyplot as plt
 import scipy.signal as sps
-from matplotlib.patches import Rectangle
 
 from openseize.types import mixins
+from openseize.types.producer import producer
 from openseize.filtering.viewer import FilterViewer
+from openseize.tools import numerical
 
 class IIR(abc.ABC, mixins.ViewInstance, FilterViewer):
     """Abstract Infinite Impulse Response Filter defining required and common
@@ -33,9 +33,8 @@ class IIR(abc.ABC, mixins.ViewInstance, FilterViewer):
         -- Computed --
         order (int):                    number filter coeffs to achieve pass,
                                         stop and transition width criteria
-        coeffs (arr):                   filter coeffs of the designed filter
-                                        "h(n)"
-        others:                         Each FIR may add additional computed
+        coeffs (arr):                   coeffs of IIR transfer function
+        others:                         Each IIR may add additional computed
                                         attrs
     """
 
@@ -54,63 +53,57 @@ class IIR(abc.ABC, mixins.ViewInstance, FilterViewer):
         self.fmt = fmt
 
     @abc.abstractmethod
-    def _build(self, fmt):
-        """Builds and returns the coefficients of this IIR filter."""
-
-    @abc.abstractmethod
     def _order(self):
         """Determines the lowest order filter to meet the pass &
         stop band attenuation criteria."""
 
-    def apply(self, arr, axis=-1, phase_shift=True, **kwargs):
+    @abc.abstractmethod
+    def _build(self, fmt):
+        """Builds and returns the coefficients of this IIR filter."""
+
+    def apply(self, x, chunksize, axis=-1, phase=True, **kwargs):
         """Apply this IIR to an ndarray of data along axis.
 
         Args:
-            arr (ndarray):          array with sample along axis
-            axis (int):             axis to filter along
-            phase_shift (bool):     boolean to compensate for IIR phase
-                                    shift (Default=True)
-            kwargs:                 passed to sosfilt
+            x (producer or array-like):          
         """
 
-        #FIXME Handle large data with a generator using add-overlap meth
-        #FIXME Handle when fmt is not sos
-        if phase_shift:
-            return sps.sosfiltfilt(self.coeffs, arr, axis=axis, **kwargs)
-        else:
-            return sps.sosfilt(self.coeffs, arr, axis=axis, **kwargs)
+        gen = numerical.batch_sosfilt(self.coeffs, x, chunksize, axis=axis)
+        return producer(gen, chunksize=chunksize, axis=axis)
 
 
 class DIIR(IIR):
-    """A Digital Infinite Impulse Response filter represented in second-
-    order sections (sos) format.
+    """A Digital Infinite Impulse Response filter.
 
-    IIR can create and apply filters of type; Butterworth, ChebyshevI,
+    DIIR can create and apply filters of type; Butterworth, ChebyshevI,
     ChebyshevII, and Elliptical. The filter order is determined as
-    the minimum order needed to meet the pass band gain 'gpass' while also
-    meeting the minimum attenuation in the stop band 'gstop'.
+    the minimum order needed to meet the pass band ripple while also
+    meeting the minimum attenuation in the stop band.
 
     Attrs:
-        cutoff (float or 1-D array):        freqs at which Gain of filter
-                                            drops to <= gstop
-        width (int):                        width of transition bewteen pass
-                                            & stop bands
-        fs (int):                           sampling rate in Hz
-        btype (str):                        filter band type one of
-                                            {lowpass, highpass, bandpass, 
-                                            bandstop}
-        ftype (str):                        a filter type must be one of
-                                            {butter, cheby1, cheby2, ellip}
-        gpass (float):                      maximum allowed attenuation in
-                                            the pass band in dB (Default is 
-                                            0.9 dB ~ 9% amplitude diff)
-        gstop (float):                      minimum attenuation in the stop
-                                            band (Default is 6dB ~ 50% amplitude
-                                            diff)
-        kwargs:                             passed to scipy iirfilter. These
-                                            should include the pass and stop 
-                                            ripples ('rp' & 'rs) for Chebyshev
-                                            and Elliptical filter types
+        fs (int):                       sampling frequency in Hz
+        nyq (int):                      nyquist frequency
+        cutoff (float or 1-D array):    freqs at which Gain of the filter
+                                        drops to <= -6dB
+        norm_cutoff:                    cutoff normed to nyquist
+        width (int):                    width of transition bewteen pass and
+                                        stop bands
+        btype (str):                    type of filter must be one of
+                                        {lowpass, highpass, bandpass, 
+                                        bandstop}. Default is lowpass
+        pass_ripple (float):            the maximum deviation in the pass
+                                        band (Default=0.005 => 0.5% ripple)
+        stop_db (float):                minimum attenuation to achieve at
+                                        end of transition width in dB 
+                                        (Default=40 dB ~ 99% amplitude
+                                        reduction)
+        fmt (str):                      representation format for coeffs 
+                                        of filter (Default is second-order 
+                                        sections 'sos')
+        -- Computed --
+        order (int):                    number filter coeffs to achieve pass,
+                                        stop and transition width criteria
+        coeffs (arr):                   coeffs of IIR transfer function
 
     Note: In this implementation all transition bands use the same width.
     Call iirfilter directly if asymmetric bands are needed.
@@ -181,20 +174,22 @@ class DIIR(IIR):
             raise ValueError(msg.format(self.ftype, forders.keys()))
 
     def _build(self):
-        """Build this digital filter using the second order section fmt."""
+        """Build this digital filter using this filter's fmt."""
 
-        return sps.iirfilter(self.order, self.cutoff, btype=self.btype, 
-                            ftype=self.ftype, output=self.fmt, fs=self.fs)
+        return sps.iirfilter(self.order, self.cutoff, rp=self._gpass,
+                    rs=self.stop_db, btype=self.btype, ftype=self.ftype, 
+                    output=self.fmt, fs=self.fs)
 
 
 
 if __name__ == '__main__':
 
-    iir = DIIR([100, 200], width=30, fs=1000, btype='bandpass', ftype='butter',
+    import matplotlib.pyplot as plt
+    
+    iir = DIIR(50, width=10, fs=5000, btype='lowpass', ftype='butter',
             pass_ripple=0.005, stop_db=40, fmt='sos')
 
     #iir.view()
-    """
     time = 10
     fs = 5000
     nsamples = int(time * fs)
@@ -203,14 +198,25 @@ if __name__ == '__main__':
     # make a small 10 Hz riding on top of a larger 100 Hz sinusoidal signal
     x = 0.5 * np.sin(2 * np.pi * 10 * t) + np.sin(2 * np.pi * 100 * t) + \
             0.25*np.random.random(nsamples)
-    y = 2 * np.sin(2 * np.pi * 10 * t) + 0.5 *np.random.random(nsamples) 
+    y = 2 * np.sin(2 * np.pi * 22 * t) + 0.5 *np.random.random(nsamples) 
 
-    arr = np.stack((x,y, 1.5*x, y))
+    arr = np.stack((x, y))
 
-    results = f.apply(arr, phase_shift=True)
+    pro = iir.apply(arr, chunksize=10000, axis=-1)
+    results = np.concatenate([arr for arr in pro], axis=-1)
 
-    fig, axarr = plt.subplots(4,1)
+    spresults = sps.sosfilt(iir.coeffs, arr, axis=-1)
+
+    print(np.allclose(results, spresults))
+
+    plt.ion()
+    fig, axarr = plt.subplots(2,1)
     [axarr[idx].plot(row) for idx,row in enumerate(arr)]
     [axarr[idx].plot(row, color='r') for idx, row in enumerate(results)]
-    plt.show()
+
     """
+    fig1, axarr1 = plt.subplots(2,1)
+    [axarr1[idx].plot(row) for idx,row in enumerate(arr)]
+    [axarr1[idx].plot(row, color='r') for idx, row in enumerate(spresults)]
+    """
+
