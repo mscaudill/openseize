e46355d467d1032e858ecdc00051a9b1894cb5bb
mscaudill <mscaudill@gmail.com>
2022-01-07 17:00:29 -0600

    refactored writers and confirmed it runs but needs testing

--- io/writers/writers.py
+++ io/writers/writers.py
@@ -1,23 +1,66 @@
+"""Context managers for writing EEG data to a variety of file types.
+
+This module defines a collection Writer types for writing EEG files from
+arrays and Reader like objects to EDF files. Each file type specific writer
+is expected to inherit the Writer ABC and override its write abstract
+method.
+
+Typical usage example:
+
+#create an EDF reader to an edf file stored on disk 
+data = Readers.EDF(<*.edf path>)
+
+#select a subset of channels
+chs = [0,3]
+
+#write a new EDF file to save path containing only a subset of original chs
+with EDF(<save path>) as outfile:
+    outfile.write(header, data, channels=chs)
+"""
+
 import abc
 import numpy as np
 from pathlib import Path
 
-from openseize.io.headers import EDFHeader
+from openseize.io.headers import headers
+from openseize.io.headers import bytemaps
 from openseize.core import mixins
+
 
 class Writer(abc.ABC, mixins.ViewInstance):
     """An ABC that defines all subclasses as context managers and describes
     required methods and properties."""
 
     def __init__(self, path, mode):
-        """Initialize this Writer with a path & a write mode 'w' or 'wb'."""
+        """Initialize this Writer.
+
+        Args:
+            path: str or Path instance
+                A path where edf file will be written to.
+            mode: str
+                A mode string describing if data is text format ('w') or
+                binary ('wb') format. 
+        """
 
         self.path = Path(path)
         self._fobj = open(path, mode)
 
     @abc.abstractmethod
     def write(self, header, data, channels, **kwargs):
-        """Writes a header and data for channels to a file obj."""
+        """Writes a header and numerical data for each channel in channels
+        to this Writer's opened file instance.
+
+        Args:
+            header: dict
+                A mapping of file specific metadata. See io.headers for
+                further details.
+            data: array or reader like
+                A 2-D array instance, memmap file or Reader like iterable
+                that returns arrays of shape channels X samples.
+            channels: sequence
+                A sequence of channel indices that will be written to this
+                Writer's save path.
+        """
 
     def __enter__(self):
         """Return instance as target variable of this context."""
@@ -36,24 +79,27 @@ class Writer(abc.ABC, mixins.ViewInstance):
         self._fobj.close()
 
 
-class EDF(Writer):
+class EDFWriter(Writer):
     """A writer of European Data Format (EDF) files.
 
     This writer does not support writing annotations to an EDF file.
     """
 
     def __init__(self, path):
-        """Initialize this Writer with a write path."""
+        """Initialize this writer. See base class for futher details."""
 
         super().__init__(path, mode='wb')
 
     def _write_header(self, header):
-        """Write header dict to header section of this Writer's fileobj.
+        """Write a header dict to this Writer's opened file instance.
 
         Args:
-            header (dict):          a dict to use as this files header
+            header: dict          
+                A dict of EDF compliant metadata. Please see 
+                io.headers.EDFHeader for further details.
         """
        
+        """
         #build EDFheader & convert all values to list
         self.header = EDFHeader.from_dict(header)
         lsheader = {k: v if isinstance(v, list) else [v]
@@ -65,39 +111,62 @@ class EDF(Writer):
         for ls, (cnts, _) in ((lsheader[k], bmap[k]) for k in bmap):
             b = [bytes(str(x), 'ascii').ljust(n) for x, n in zip(ls, cnts)]
             self._fobj.write(b''.join(b))
+        """
+
+        self.header = header
+        bytemap = bytemaps.edf(header.num_signals)
+        # Move to file start and write each ascii encoded byte string
+        self._fobj.seek(0)
+        for items, (nbytes, _) in zip(header.values(), bytemap.values()):
+            items = [items] if not isinstance(items, list) else items
+            for item, nbyte in zip(items, nbytes):
+                bytestr = bytes(str(item), encoding='ascii').ljust(nbyte)
+                self._fobj.write(bytestr)
 
     def _records(self, data, channels):
-        """A generator yielding list of samples one per channel for a single
-        record extracted from data.
+        """Yields a list of sample arrays one per channel to write to
+        a single data record
 
         Args:
-            data (ndarray, reader):     an ndarray, memmap or reader
-                                        instance with shape channels x 
-                                        samples 
-            channels (list):            list of channels to write
+            data: 2-D array, memmap, Reader instance
+                An object that is sliceable or has a read method that 
+                returns arrays of shape channels x samples. 
+            channels: sequence
+                A sequence of channels to write to each data record in this
+                Writer.
 
-        Yields: list of arrays of samples one per channel that constitute
-                a record
+        Yields:
+            A list of 1-D arrays of samples for a single data record, one
+            array per channel in channels.
         """
 
         for n in range(self.header.num_records):
             result = []
-            #compute the start and stop sample for each channel
+            # The number of samples per record is channel dependent if
+            # sample rates are not equal across channels.
             starts = n * np.array(self.header.samples_per_record)
             stops = (n+1) * np.array(self.header.samples_per_record)
+
             for channel, start, stop in zip(channels, starts, stops):
-                #fetch from array or reader and yield
                 if isinstance(data, np.ndarray):
                     result.append(data[channel][start:stop])
                 else:
                     result.append(data.read(start, stop, [channel]))
+            
             yield result
 
     def _encipher(self, arrs):
-        """Transform each 1-D array in arrays from floats to an array
-        of 2-byte little-endian integers.
+        """Transforms each array in a sequence of arrays from float to
+        a 2-byte little-endian integer dtype.
 
-        see: _decipher method of the EDFReader.
+        Args:
+            arrs: sequence
+                Sequence of 1-D arrays of float dtype.
+
+        See also _decipher method of the EDFReader.
+
+        Returns:
+            A sequence of 1-D arrays in 2-byte little-endian fmt.
         """
 
         slopes = self.header.slopes
@@ -110,52 +179,68 @@ class EDF(Writer):
         return results
 
     def _validate(self, header, data):
-        """Validates that the number of samples to be written is divisible
-        by the number of records in the header."""
+        """Validates that samples in data is divisible by number of records
+        in header.
+
+        The EDF file spec. does not allow the writing of partial record at
+        the end of the file. Therefore, the data to be written needs to 
+        perfectly fill all num_records in the header. This may require 
+        appending 0's to the end of data to ensure this.
+
+        Args:
+            header: dict
+                An EDFHeader instance.
+            data: 2-D array or Reader instance
+                Data to be written to this Writer's open file instance.
+        
+        Raises:
+            A ValueError if num. samples is not divisible by num. records 
+            in header. 
+        """
 
         if data.shape[1] % header.num_records != 0:
             msg=('Number of data samples must be divisible by '
                  'the number of records; {} % {} != 0')
             raise ValueError(msg.format(values, num_records))
 
-    def _progress(self, idx):
+    def _progress(self, record_idx):
         """Relays write progress during file writing."""
-
+        
         msg = 'Writing data: {:.1f}% complete'
-        perc = idx / self.header.num_records * 100
+        perc = record_idx / self.header.num_records * 100
         print(msg.format(perc), end='\r', flush=True) 
 
     def write(self, header, data, channels, verbose=True):
-        """Writes the header and data to write path.
+        """Write header & data for each channel to file object.
 
         Args:
-            header (dict):              dict containing required items
-                                        of an EDF header (see io.headers)
-            data (ndarray, reader):     an ndarray, memmap or reader
-                                        instance with shape channels x 
-                                        samples 
-            channels (list):            list of channels to write
-            verbose (bool):             display write percentage complete
+            header: dict
+                A mapping of EDF compliant fields and values. For Further
+                details see EDFHeader in io.headers module.
+            data: 2-D array or Reader instance
+                A channels x samples array or Reader instance.
+            channels: sequence
+                A sequence of channel indices to write to this Writer's 
+                open file instance.
+            verbose: bool
+                An option to print progress of write. Default (True) prints
+                status update as each record is written.
         """
 
-        #build & write header
-        header = EDFHeader.from_dict(header)
-        #filter the header to include only channels
-        header = header.filter('channels', channels)
-        #before write validate
+        header = headers.EDFHeader.from_dict(header)
+        header = header.filter(channels)
         self._validate(header, data)
+
         self._write_header(header)
-        #Move to data records section, fetch and write records
         self._fobj.seek(header.header_bytes)
         for idx, record in enumerate(self._records(data, channels)):
-            samples = self._encipher(record)
+            samples = self._encipher(record) # floats to '<i2'
             samples = np.concatenate(samples)
-            #convert to bytes and write to file obj
+            #concatenate data bytes to str and write
             byte_str = samples.tobytes()
             self._fobj.write(byte_str)
             if verbose:
                 self._progress(idx)
-        print('')
 
 
         
@@ -169,12 +254,12 @@ if __name__ == '__main__':
     path2 = '/home/matt/python/nri/data/openseize/test_write.edf'
     
 
-    reader = readers.EDF(path)
+    reader = readers.EDFReader(path)
     header = reader.header
 
     t0 = time.perf_counter()
-    with EDF(path2) as writer:
-        writer.write(header, reader, channels=[0,1,2,3])
+    with EDFWriter(path2) as writer:
+        writer.write(header, reader, channels=[0,1])
     print('Written in {} s'.format(time.perf_counter() - t0))
 
 
