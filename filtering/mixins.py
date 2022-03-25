6e22cf427b698a3444415828187572bc745f2a4a
mscaudill <mscaudill@gmail.com>
2022-03-25 15:53:56 -0500

    completed IIRDesign for plotting impulse and frequency responses of IIR filters and placed into base inheritance family.

--- filtering/mixins.py
+++ filtering/mixins.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 from matplotlib.patches import Rectangle
 
-class IIRViewer:
+class IIRDesign:
     """A mixin for IIR filters with methods for plotting the impulse
     response, frequency response and phase responses of an IIR filter."""
 
@@ -35,7 +35,7 @@ class IIRViewer:
                 response over. Default is 512.
             rope: float
                 For plotting, all values below this region of pratical
-                equivalence will be set to the rope value. Default is -100
+                equivalence will be set to this value. Default is -100
                 dB. Any filter response smaller than this will be set to
                 -100 for plotting.
 
@@ -69,91 +69,162 @@ class IIRViewer:
         time = np.linspace(0, 1, self.fs)
         imp_response = self.impulse_response()
         ax.plot(time, imp_response, **kwargs)
+        order, wn = self.order
+        ax.text(.76, .6, 'Filter Order = {}'.format(order), 
+                transform=ax.transAxes, fontweight='bold', color='tab:blue')
 
-    def _plot_response(self, ax, freqs, response, scale, alpha=0.2, **kwargs):
-        """ """
+    def _band_coords(self, ax, scale):
+        """Returns a list of coordinates for each pass and transition band
+        rectangle for plotting.
 
-        ax.plot(freqs, response, **kwargs)
+        Args:
+            ax: matplotlib axis instance
+                The axis where pass and tranisition bands will be plotted.
+            scale: str
+                The units of the gain response being plotted to ax. Must be
+                one of 'dB' or 'abs' for decibels and relative magnitude
+                respectively.
 
-        # get coordinates of pass and transition bands
+        Returns: a list of tuple sequence arguments for matplotlib's
+                 Rectangle patch one per band to be plotted.
+        """
+
         if self.btype == 'lowpass':
-            pass_start = np.array([0])
-            pass_width = self.fpass
-            trans_start = self.fpass
-            trans_width = self.fstop - trans_start
-
+            pass_bands = np.array([[0, self.fpass[0]]])
+            trans_bands = np.array([[self.fpass[0], self.fstop[0]]])
+        
         elif self.btype == 'highpass':
-            pass_start = self.fpass
-            pass_width = np.atleast_1d(self.nyq) - pass_start
-            trans_start = self.fstop
-            trans_width = self.fpass - trans_start
+            pass_bands = np.array([[self.fpass[0], self.nyq]])
+            trans_bands = np.array([[self.fstop[0], self.fpass[0]]])
 
         elif self.btype == 'bandpass':
-            pass_start = np.atleast_1d(self.fpass[0])
-            pass_width = np.atleast_1d(self.fpass[1] - pass_start)
-            # two transition bands for bandpass
-            trans_start = np.array([self.fstop[0], self.fpass[1]])
-            trans_width = np.array([self.fpass[0] - self.fstop[0],
-                                    self.fstop[1] - self.fpass[1]])
+            pass_bands = np.atleast_2d(self.fpass)
+            #bandpass has two transition bands
+            trans_bands = np.array([[self.fstop[0], self.fpass[0]],
+                                   [self.fpass[1], self.fstop[1]]])
 
         elif self.btype == 'bandstop':
-            # two passband starts and widths for bandstop
-            pass_start = np.array([0, self.fstop[1]])
-            pass_width = np.array([self.fstop, ax.get_xlim()[1]])
+            #bandstop has two pass and transition bands
+            pass_bands = np.array([[0, self.fpass[0]], 
+                                  [self.fpass[1], self.nyq]])
+            trans_bands = np.array([[self.fpass[0], self.fstop[0]],
+                                   [self.fstop[1], self.fpass[1]]]) 
 
+        # Get bottom and top of pass and trans bands
         bottom = ax.get_ylim()[0]
         top = 0 if scale=='dB' else 1
         height = top - bottom
+    
+        # returned coords are (left, bottom), width, height for each band
+        pc = [[(p[0], bottom), p[1] - p[0], height] for p in pass_bands]
+        tc = [[(t[0], bottom), t[1] - t[0], height] for t in trans_bands]
 
+        # get the pass attenuation coords
+        abottom = -self.gpass if scale=='dB' else 10**(-self.gpass/20)
+        atop = self.gpass if scale=='dB' else 10**(self.gpass/20)
+        aheight = atop - abottom
+
+        # returned coords are (left, bottom), width, height for each band
+        ac = [[(p[0], abottom), p[1] - p[0], aheight] for p in pass_bands]
+
+        return pc, tc, ac
+
+    def _plot_response(self, ax, freqs, response, scale, **kwargs):
+        """Plots the frequency response of this filter and the pass and
+        transition bands.
+
+        Args:
+            ax: matplotlib axis instance
+                The axis to plot the response to.
+            freqs: 1-D array
+                The frequencies at which the response was computed.
+            response: 1-D array
+                The response of this filter at each frequency.
+            scale: str
+                The units of the response. Should be one of 'dB' or 'abs'
+                for decibels and relative magnitudes respectively.
+            kwargs: dict
+                Any valid kwarg to matplotlib plot for adjusting line
+                properties. The rectangular band properties are not
+                configurable.
+
+        Returns: The input ax with plots and rectangles added.
+        """
+
+        ax.plot(freqs, response, **kwargs)
+
+        pass_coords, trans_coords, att_coords = self._band_coords(ax, scale)
+        
         # construct passband rectangles and place on  axis
-        pass_rects = [Rectangle((start, bottom), width, height,
-                                 facecolor='tab:green', alpha=alpha) 
-                      for start, width in zip(pass_start, pass_width)]
+        pass_rects = [Rectangle(*pass_coord, fc='tab:green', alpha=0.2) 
+                      for pass_coord in pass_coords]
         [ax.add_patch(rect) for rect in pass_rects]
 
         # construct transition rectangles and place on axis
-        trans_rects = [Rectangle((start, bottom), width, height,
-            facecolor='red', alpha=alpha) for start,
-                                 width in zip(trans_start, trans_width)]
-
+        trans_rects = [Rectangle(*trans_coord, fc='red', alpha=0.2) 
+                       for trans_coord in trans_coords]
         [ax.add_patch(rect) for rect in trans_rects]
-       
-        # construct a rectangle for the pass band attenuation
-        attn_bottom = -self.gpass if scale=='dB' else 10**(-self.gpass/20)
-        attn_top = self.gpass if scale=='dB' else 10**(self.gpass/20)
-        attn_height = attn_top - attn_bottom
-        attn_rects = [Rectangle((start, attn_bottom), width, attn_height,
-                                 facecolor='None', edgecolor='gray',
-                                 linestyle='dotted') 
-                      for start, width in zip(pass_start, pass_width)]
+
+        # construct attenuation rectangle in pass bands
+        attn_rects = [Rectangle(*att_coord, fc='None', edgecolor='gray',
+                                linestyle='dotted') 
+                      for att_coord in att_coords]
         [ax.add_patch(rect) for rect in attn_rects]
 
         return ax
 
     def plot(self, size=(8,6), gridalpha=0.3, worN=2048):
-        """ """
+        """Plots the impulse and frequency response of this IIR filter.
+
+        Args:
+            size: tuple
+                The figure size to display for the plots. Default is 8 x 6.
+            gridalpha: float in [0, 1]
+                The alpha transparency of each subplots grid. Default is 0.3
+            worN: int
+                The number of frequencies to compute the gain and phase
+                responses over. Default is 2048 frequencies.
+        """
 
         fig, axarr = plt.subplots(3, 1, figsize=size)
+        axarr[1].get_shared_x_axes().join(axarr[1], axarr[2])
         
         # Impulse response
         self._plot_impulse(axarr[0], color='tab:blue')
+        axarr[0].set_xlabel('Time (s)', fontweight='bold')
+        axarr[0].set_ylabel('Amplitude (au)', color='k',
+                            fontweight='bold')
  
         # Frequency response in dB
         freqs, gain_dB, scale = self.frequency_response(scale='dB')
         self._plot_response(axarr[1], freqs, gain_dB, scale,
                 color='tab:blue')
+        axarr[1].set_ylabel('Gain (dB)', color='tab:blue',
+                            fontweight='bold')
+        axarr[1].xaxis.set_ticklabels([])
 
+        # Phase response of this filter
         ax2 = axarr[1].twinx()
         freqs, gainz, scale = self.frequency_response(scale='complex')
         angles = np.unwrap(np.angle(gainz, deg=True))
-        ax2.plot(freqs, angles, color='k', alpha=0.2)
+        ax2.plot(freqs, angles, color='tab:orange', alpha=0.5)
+        ax2.spines['top'].set_visible(False)
+        ax2.set_ylabel('Phase ($^\circ$)', color='tab:orange',
+                        fontweight='bold')
         
-        # Frequency response in magnitude
+        # Frequency response in relative magnitude
         freqs, gain_abs, scale = self.frequency_response(scale='abs')
         self._plot_response(axarr[2], freqs, gain_abs, scale, color='tab:blue')
-        
+        axarr[2].set_xlabel('Frequency (Hz)', fontweight='bold')
+        axarr[2].set_ylabel('Gain (au)', color='k', fontweight='bold')
+
+        # Axes configurations
         [ax.grid(alpha=gridalpha) for ax in axarr]
+        axarr[0].spines['right'].set_visible(False)
+        axarr[0].spines['top'].set_visible(False)
+        axarr[1].spines['top'].set_visible(False)
+        axarr[2].spines['right'].set_visible(False)
+        axarr[2].spines['top'].set_visible(False)
         
-        plt.ion()
         plt.tight_layout()
         plt.show()
