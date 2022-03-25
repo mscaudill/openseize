65acbad1221ee0151af5bf601a0c890e8f79c5a9
mscaudill <mscaudill@gmail.com>
2022-03-25 15:55:12 -0500

    main changes to preview IIR band types

--- filtering/iir.py
+++ filtering/iir.py
@@ -34,7 +34,8 @@ if __name__ == '__main__':
 
     arr = np.stack((x, y))
 
-    butter = Butter(fpass=[400], fstop=[500], fs=fs) #lowpass
+    #butter = Butter(fpass=[400], fstop=[500], fs=fs) #lowpass
     #butter = Butter(fpass=[500], fstop=[400], fs=fs) #highpass
-    #butter = Butter(fpass=[500, 700], fstop=[400, 800], fs=fs) #bandpass
+    butter = Butter(fpass=[500, 700], fstop=[400, 800], fs=fs) #bandpass
+    #butter = Butter(fpass=[400, 800], fstop=[500, 700], fs=fs) #bandstop
 
