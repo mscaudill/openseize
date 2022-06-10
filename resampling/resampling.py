4b4db966d09d3e15e72c1c462e00c2e0c6f123f7
mscaudill <mscaudill@gmail.com>
2022-06-10 16:34:14 -0500

    added support for multichannel data

--- resampling/resampling.py
+++ resampling/resampling.py
@@ -90,22 +90,42 @@ def downsample(arr, fs, M, axis=-1):
     #result = sp.signal.oaconvolve(p, u, axes=-1)
     return result
 
-def ndownsample(arr, fs, M axis=-1):
+def ndownsample(arr, fs, M, axis=-1):
     """Ver. 3 Downsamples multichannel data."""
 
+    x = arr.T if axis == 0 else arr
 
-    
+    fstop = fs // M
+    # FIXME is this going to work always?
+    fpass = fstop - fstop // 10 
 
+    gpass, gstop = 1, 40
 
+    filt = kaiser(fpass, fstop, fs=fs, gpass=gpass, gstop=gstop)
+    h = filt.coeffs
 
+    # u_m are advanced in time from m=0 upto M-1, so pad
+    padded = arraytools.pad_along_axis(x, [M-1,0], axis=1)
 
+    # polyphase components of h
+    p_len = int(np.ceil(len(h) / M))
+    p = arraytools.pad_along_axis(h, [0, M * p_len - len(h)], axis=0)
+    p = p.reshape((M, p_len), order='F') 
 
+    # shifted decimations
+    u_len = int(np.ceil(padded.shape[1] / M)) 
 
+    # build array chs x M x u_len
+    u = arraytools.pad_along_axis(padded, [0, M * u_len - padded.shape[1]],
+                                  axis=1)
+    u = u.reshape((x.shape[0], M, u_len), order='F')
+    u = np.flip(u, axis=1)
 
-
-
-
-
+    result = np.zeros((x.shape[0], (u_len + p_len - 1)))
+    for idx in range(x.shape[0]):
+        for m in range(M):
+            result[idx] += np.convolve(p[m], u[idx, m])
+    return result
 
 
 
@@ -116,23 +136,26 @@ if __name__ == '__main__':
 
     edf_path = '/home/matt/python/nri/data/openseize/CW0259_P039.edf'
 
-    arr = data(edf_path, 0, 80000000)
-    signal = arr[0]
+    arr = data(edf_path, 0, 30000000)
 
-    
+    # polyphase decomposition
     t0 = time.perf_counter()
-    d = downsample(signal, M=10, fs=5000)
+    d = ndownsample(arr, M=10, fs=5000)
     print('Polyphase time = {}'.format(time.perf_counter() - t0))
 
+    # Standard method 
     t0 = time.perf_counter()
-    filt = kaiser(fpass=500, fstop=100, fs=5000)
-    ground_truth = np.convolve(signal, filt.coeffs, mode='full')
-    ground_truth = ground_truth[::10]
+    filt = kaiser(fpass=450, fstop=500, fs=5000)
+    ground_truth = []
+    for signal in arr:
+        x = np.convolve(signal, filt.coeffs, mode='full')
+        ground_truth.append(x[::10])
+    ground_truth = np.array(ground_truth)
     print('Filter-decimate time = {}'.format(time.perf_counter() - t0))
 
-    """
-    plt.plot(d)
-    plt.plot(ground_truth)
+   
+    fig, axarr = plt.subplots(arr.shape[0], 1)
+    for idx, (a, b) in enumerate(zip(d, ground_truth)):
+        axarr[idx].plot(a[100000:200000], color='tab:blue')
+        axarr[idx].plot(b[100000:200000], color='tab:orange', alpha=0.5)
     plt.show()
-    """
-    
