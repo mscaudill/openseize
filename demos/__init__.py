f858392e0d9e2173f7a8f132f8b1c81f60df6b67
mscaudill <mscaudill@gmail.com>
2022-07-05 08:28:43 -0500

    modifying init to compute available paths on demo import

--- demos/__init__.py
+++ demos/__init__.py
@@ -1,15 +1,4 @@
-import inspect
-from pathlib import Path
-
-def demopaths():
-    """Returns all the paths in the data dir of the demo module of
-    openseize."""
-
-    #get path to the demo data dir
-    current = Path(inspect.getabsfile(inspect.currentframe()))
-    data_dir = current.parent.joinpath('data')
-    return {path.name: path for path in data_dir.iterdir()}
-
+from openseize.demos import paths
 
 # make demo data paths available in demos namespace
-paths = demopaths()
+datapaths = paths.Datapaths()
