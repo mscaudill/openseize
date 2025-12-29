[bugfix]: ensure window is at least 2D for convolution shape computation

src/openseize/filtering/bases.py
@@ -385,7 +385,7 @@ class FIR(abc.ABC, mixins.ViewInstance, FIRViewer):
         """
 
         pro = producer(data, chunksize, axis, **kwargs)
-        window = self.coeffs
+        window = np.atleast_2d(self.coeffs)
 
         # construct overlap-add generating function & get resultant shape
         genfunc = partial(nm.oaconvolve, pro, self.coeffs, axis, mode)