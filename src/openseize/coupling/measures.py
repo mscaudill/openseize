fb841d36d94a14582c59d812e9e530c2632b1d5d
mscaudill <mscaudill@gmail.com>
2026-01-06 13:36:33 -0600

    [wip]: continuing work on measures, found high variability on epsi parameter

--- src/openseize/coupling/measures.py
+++ src/openseize/coupling/measures.py
@@ -1,6 +1,6 @@
 """Estimators of Cross-Frequency Coupling (CFC)."""
 
-from collections.abc import Sequence
+from collections.abc import Sequence, Iterator
 import multiprocessing as mp
 from itertools import zip_longest
 from functools import partial
@@ -35,45 +35,71 @@ class PhasePowerLocking:
     ) -> None:
         """Initialize this Estimator."""
 
-        self.fs = fs
-        self.csize = chunksize
-        self.axis = axis
+        self._fs = fs
+        self._csize = chunksize
+        self._axis = axis
 
-    def indices(
+    @property
+    def fs(self):
+        """Returns the immutable sampling rate of this estimator."""
+
+        return self._fs
+
+    @property
+    def chunksize(self):
+        """Returns the immutable chunksize of this estimator."""
+
+        return self._csize
+
+    @property
+    def axis(self):
+        """Returns the immutable axis attribute of this estimator."""
+
+        return self._axis
+
+
+    # TODO DOCS
+    def phase_indices(
         self,
         signal: Producer | npt.NDArray,
         fpass: list[float, float],
         fstop: list[float, float],
         analytic_width: float = 4,
         angle: float = 0,
-        epsi: float = 0.1,
+        epsi: float = 0.05,
         **kwargs
     ) -> list[npt.NDArray]:
         """Estimates the phase indices at which powers will be measured."""
 
-        pro = producer(signal, chunksize=self.csize, axis=self.axis)
-        if all(np.array(pro.shape) > 1):
+        pro = producer(signal, chunksize=self.chunksize, axis=self.axis)
+        if pro.ndim > 2 or min(pro.shape) > 1:
             'Signal to estimate phase indices must be 1D'
             raise ValueError(msg)
 
         filt = fir.Kaiser(fpass, fstop, self.fs, **kwargs)
-        x = filt(pro, chunksize=self.csize, axis=self.axis)
-        analytic = Analytic(x, chunksize=self.csize, axis=self.axis)
+        x = filt(pro, chunksize=self.chunksize, axis=self.axis)
+        analytic = Analytic(x, chunksize=self.chunksize, axis=self.axis)
         analytic.estimate(width=analytic_width, fs=self.fs)
-        indices = np.squeeze(analytic.indices(analytic.phases, angle, epsi))
 
-        return np.squeeze(indices)
+        indices = []
+        for arr in analytic.phases:
+            phi = np.squeeze(arr)
+            near = np.logical_and(phi > angle - epsi, phi < angle + epsi)
+            indices.append(np.flatnonzero(near))
 
-    def _reindex(self, indices, chunksize):
+        return indices
+
+    def shuffles(indices, signal, seed, count):
         """ """
 
-        rel = np.mod(indices, chunksize)
-        flips = np.flatnonzero(np.diff(rel, prepend=rel[0]) <= 0)
-        slices = (slice(a, b) for a, b in zip_longest(flips, flips[1:]))
+        # compute linear index indices from chunked indices
+        # shift and sort
+        # rechunk them and yied
+        pass
 
-        return [indices[sl] for sl in slices]
 
-    def _shifts(self, signal, indices, size, seed):
+    """
+    def shuffles(self, signal, indices, rng, count):
         """ """
 
         pts = signal.shape[self.axis]
@@ -83,13 +109,15 @@ class PhasePowerLocking:
 
         for shift in shifts:
             yield np.mod(indices + shift, signal.shape[self.axis])
+    """
 
     #TODO move centers, bandwidth, analytic width, gpass, gstop to init
     # or maybe require a filter to be given to init
     def _estimate(
         self,
         signal: Producer,
-        indices: npt.NDArray,
+        indices: list[npt.NDArray], #next use gen of list of arrays for shuffs
+        shuffles: Iterator[list[npt.NDArray]] | None,
         center: float,
         bandwidth: float,
         analytic_width: float,
@@ -102,14 +130,40 @@ class PhasePowerLocking:
 
         fpass = center + np.array([-bandwidth/2, bandwidth/2])
         fstop = fpass + np.array([-bandwidth/2, bandwidth/2])
-        #TODO chk gpass and gstop
-        filt = fir.Kaiser(fpass, fstop, self.fs, gpass=0.1, gstop=40)
-        x = filt(signal, chunksize=self.csize, axis=self.axis)
+        filt = fir.Kaiser(fpass, fstop, self.fs, gpass=0.01, gstop=60)
+        x = filt(signal, chunksize=self.chunksize, axis=self.axis)
         z = protools.standardize(x, axis=self.axis)
 
-        analytic = Analytic(z, chunksize=self.csize, axis=self.axis)
+        analytic = Analytic(z, chunksize=self.chunksize, axis=self.axis)
         analytic.estimate(width=analytic_width, fs=self.fs)
 
+        window = np.array([-winsize/2, winsize/2]).astype(int)
+        avg, cnt = 0, 0
+
+        for amps, phis in zip(analytic.amplitudes, indices):
+            x = np.squeeze(amps)
+
+            # as chunksize changes this may lead to slightly different results
+            # as some windows are dropped
+            for phi in phis:
+                new_power = x[slice(*(window + phi))] ** 2
+                if len(new_power) < winsize:
+                    continue
+
+                avg = (cnt * avg + new_power) / (cnt + 1)
+                cnt += 1
+
+        """
+        for shuffle in self.shuffles(indices, signal, n):
+            # do the same as above and refactor
+            pass
+        """
+
+        return avg
+
+
+        """
+        # FIXME and if no surrogates n=None is passed...
         surrogates = []
         for it, ixs in enumerate(self._shifts(signal, indices, size=n,
             seed=seed), 1):
@@ -135,31 +189,10 @@ class PhasePowerLocking:
             print(f'{it} / {n} completed')
 
         return center, surrogates[0], np.mean(surrogates[1:], axis=0), np.std(surrogates[1:], axis=0)
-
-
-
-
-
-        """
-        reindexed = self._reindex(indices, self.csize)
-
-        result = 0
-        cnt = 0
-        span = np.array([-1, 1]) * delay * self.fs
-        for amps, idxs in zip(analytic.amplitudes, reindexed):
-            x = np.squeeze(amps)
-            for idx in idxs:
-
-                y = x[slice(*(span + idx))] ** 2
-                if len(y) < 2 * delay * self.fs:
-                    continue
-
-                result = (cnt * result + y) / (cnt + 1)
-                cnt += 1
-
-        return center, result
         """
 
+
+    """
     def estimate(
         self,
         signal,
@@ -224,6 +257,7 @@ class PhasePowerLocking:
                 result[v[0]] = v[1]
 
         return result
+    """
 
 
 
@@ -240,32 +274,40 @@ if __name__ == '__main__':
     base = '/media/matt/Magnus/Qi/EEG_annotation_03272024/'
     name = 'No_6489_right_2022-02-09_14_58_21_(2)_annotations.edf'
     path = Path(base) / Path(name)
+    csize = int(10e6)
+    axis = -1
 
     x = Reader(path)
     x.channels = [3]
-    xpro = producer(x, chunksize=10e6, axis=-1)
-    dxpro = downsample(xpro, M=10, fs=5000, chunksize=int(10e6))
+    xpro = producer(x, chunksize=csize, axis=axis)
+    dxpro = downsample(xpro, M=10, fs=5000, chunksize=csize)
 
 
     y = Reader(path)
     y.channels = [2]
-    ypro = producer(y, chunksize=10e6, axis=-1)
-    dypro = downsample(ypro, M=10, fs=5000, chunksize=int(10e6))
+    ypro = producer(y, chunksize=csize, axis=axis)
+    dypro = downsample(ypro, M=10, fs=5000, chunksize=csize)
 
-    PPL = PhasePowerLocking(fs=500)
+    PPL = PhasePowerLocking(fs=500, chunksize=csize)
 
     t0 = time.perf_counter()
-    indices = PPL.indices(dxpro, fpass=(4, 12), fstop=[2, 14])
+    indices = PPL.phase_indices(dxpro, fpass=(4, 12), fstop=[2, 14], epsi=0.05)
     print(f'Phase events in {time.perf_counter() - t0} s')
 
     t0 = time.perf_counter()
-    c, power, surr_power, surr_std = PPL._estimate(dypro, indices, center=165, bandwidth=4,
-            analytic_width=4, winsize=2, n=30, seed=0)
+    power = PPL._estimate(dypro, indices, None, center=150, bandwidth=4,
+            analytic_width=4, winsize=1000, n=30, seed=0)
     print(f'Powers in {time.perf_counter() - t0} s')
 
     import matplotlib.pyplot as plt
+    plt.plot(np.squeeze(power) - np.mean(power))
+    plt.show()
+
+
+    """
     plt.plot(surr_power)
     plt.plot(surr_power + surr_std)
     plt.plot(power)
     plt.show()
+    """
 
